4132f01efe7ab0f0af4f6ac65d8038e7651138f3
Gaurav Shindolkar <gauravshindolkar@gmail.com>
2018-12-19 17:58:07 +0530

    chnaged app text

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ COPY . .
 RUN npm run build
 
 FROM nginx
+#Below Expose is for AWS Beanstalk
+EXPOSE 80
 COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Bye There!
+            I was changed on Feature
           </p>
           <a
             className="App-link"
